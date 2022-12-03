490c3243c4278bcb029dc7dbe51fcdf28c5e519b
Keve Lajsz <kevelajsz@icloud.com>
2022-12-03 09:11:01 +0100

    feat(2022): :sparkles: Day 3 task completed

--- src/year2022/day3/RucksackReorganization.java
+++ src/year2022/day3/RucksackReorganization.java
@@ -10,7 +10,7 @@ import java.util.List;
 class RucksackReorganization {
     public static void main(String[] args) throws IOException {
         // Reading data file (learnt of a new fancy way)
-        Path dataFilePath = Paths.get("src/year2022/day3/data.test.txt");
+        Path dataFilePath = Paths.get("src/year2022/day3/data.txt");
         List<String> sackContentList = Files.readAllLines(dataFilePath);
 
         // Priority string: used to determine the priority of each character
@@ -20,29 +20,43 @@ class RucksackReorganization {
 
         // Summing up the commons
         int sum = 0;
-        for (var content : sackContentList) {
-            // Getting the first half of the string as an array, in which each
-            // element is a character from the string
-            List<String> first = Arrays.asList(
-                    content.substring(0, content.length() / 2).split(""));
-            // Same, but with the second half
-            List<String> second = Arrays.asList(
-                    content.substring(content.length() / 2).split(""));
+        for (int i = 0; i < sackContentList.size(); i += 3) /* we need every third element */ {
+            // Creating a list from the 3 elements and getting the common character
+            char common = commonChar(Arrays.asList(new String[] {
+                    sackContentList.get(i), sackContentList.get(i + 1), sackContentList.get(i + 2)
+            }));
 
-            // Looping through the lists, finding the first (and only) common character
-            char common = 0;
-            for (var c : first) {
-                if (second.contains(c)) {
-                    common = c.charAt(0);
-                    break; // not needed, but makes sense here
-                }
-            }
-
-            // Using the priority string, to determine the value of the character
+            // Using the priority string to determine the value that need to be added
             sum += priorityString.indexOf(common);
         }
 
         // Printing the solution out
         System.out.println(sum);
+    }
+
+    // Finds the (only, according to the task) common character
+    // in a list of strings
+    private static char commonChar(List<String> strings) {
+        // Converting the list of strings to a character matrix
+        List<List<Character>> charMatrix = strings.stream().map(t -> {
+            return Arrays.asList(t.split("")).stream().map(u -> {
+                // This array is needed for mapping the List<String> to
+                // List<Character>
+                // It's not necessary to use the Character type, but it helps clear the
+                // confusion up a bit in my opinion between strings and chars
+                return u.charAt(0);
+            }).toList();
+        }).toList();
+
+        // Looping through the lists, finding the common character
+        for (var character : charMatrix.get(0)) {
+            // Using the Collection.stream() method, we are able to test, if
+            // a boolean statement is applicable to every element in a list
+            if (charMatrix.stream().allMatch(t -> t.contains(character))) {
+                return character;
+            }
+        }
+
+        return 0;
     }
 }
